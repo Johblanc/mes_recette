f567819cbddb738dc0ba9a352513c37e37faa66f
DE URTASUN Sokunthy (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7908391c@commun.ad.sncf.fr>
2022-12-15 22:00:50 +0100

    feat: ajout de la function par nom

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@
 
 import './App.css';
 import './customs/maquette/maquette.css';
-import { RechercheNom } from './components/Nom/rechercheNom';
 import { RechercheNom2 } from './components/Nom/test_fetch';
 import { TestConvertObject } from './components/Nom/test_object';
 import { Category } from './components/categorie/Category';
 import { NavBar } from './components/header/NavBar';
 import "./NavBar.css";
-
+import { RechercheNom } from './components/Nom/RechercheNom';
 function App() {
   return (
     <div className="App">
-      {/* <NavBar />
-      <RechercheNom />
-      <TestConvertObject /> */}
-      <Category />
+      {// <NavBar />
+      <RechercheNom/>
+      /*<TestConvertObject />*/ }
+      {/*<Category />*/}
 
     </div>
   );
