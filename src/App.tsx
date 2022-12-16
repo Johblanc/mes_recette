e4aa1628ef18b40b46f6959fae318fe412279fd4
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-12-16 13:46:25 +0100

    fix : Clean App

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@
 
 import './App.css';
 import './customs/maquette/maquette.css';
-import { RechercheNom } from './components/Nom/rechercheNom';
-import { RechercheNom2 } from './components/Nom/test_fetch';
-import { TestConvertObject } from './components/Nom/test_object';
 
 function App() {
+
+  
   return (
     <div className="App">
-        <TestConvertObject />
     </div>
   );
 }
