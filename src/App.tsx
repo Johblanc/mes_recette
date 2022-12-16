315dd05da8ef16eebb80167ccc76fc192b0574ea
Johblanc <79146321+Johblanc@users.noreply.github.com>
2022-12-16 16:34:39 +0100

    test de combinaison

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,24 @@ import { TestConvertObject } from './components/Nom/test_object';
 import { NavBar } from './components/header/NavBar';
 import { RechercheNom } from './components/Nom/rechercheNom';
 import { FilterByCategory } from './components/categorie/searchByCategory';
+import { Accueil } from './components/acceuilPage/acceuil';
+import { useState } from 'react';
 function App() {
-
+  const [primePage, setPrimePage] = useState(false)
   
   return (
     <div className="App">
       <NavBar/>
-      <RechercheNom/> 
-      {/* <TestConvertObject /> */}
-      <Category />
-      <FilterByCategory/>
-
+      {primePage && <Accueil overLaGroup={''} headerTitre={''} sousTitre={''} bouttonChercher={''} recettesYummy={''} poke1={'poke-1.png'} poke1Props={''}/>}
+      {!primePage && <div>
+        
+        <RechercheNom/> 
+        <p> ----------------------------------------------------- </p>
+        <Category />
+        <FilterByCategory/>
+        <p> ----------------------------------------------------- </p>
+        <SearchByIngredients/>
+      </div>}
     </div>
   );
 }
